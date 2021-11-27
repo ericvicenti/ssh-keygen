dee7e2ea4f87657898241510c30be7f1b22f1f20
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-11-27 18:08:17 -0400

    Add TypeScript type definitions

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+/// <reference types="node" />
+
+interface Config {
+  location?: string;
+  read?: boolean;
+  force?: boolean;
+  destroy?: boolean;
+}
+
+interface SshKeygenOptions {
+  /** Provides a new comment to `shh-keygen` utility. */
+  comment?: string;
+  /** Provides the new passphrase. */
+  password?: string;
+  /**
+   * Specifies the number of bits in the key to create. The minimum size is
+   * 1024 bits.
+   * @default '2048'
+   */
+  size?: string;
+  /**
+   * Specify a key format for key generation. Setting a format of "PEM" when
+   * generating a supported private key type will cause the key to be stored
+   * in the legacy PEM private key format.
+   * @default 'RFC4716'
+   */
+  format?: 'RFC4716' | 'PKCS8' | 'PEM';
+}
+
+/**
+ * Generates SSH key-pairs.
+ */
+export default function keygen(
+  opts: Config & SshKeygenOptions,
+  callback: (
+    errOrMessage?: string | NodeJS.ErrnoException,
+    out?: { key: string; pubKey: string },
+  ) => void,
+): void;
+
