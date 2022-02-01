aa3437caec63deac7cbc4eb82a3ffead3b90b3d3
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-01-31 23:58:59 -0400

    test: add basic test to the promise-base api

--- test.js
+++ test.js
@@ -1,13 +1,44 @@
 var keygen = require('./src/ssh-keygen');
 
-console.log('Generating key pair')
+function runCallbackVersion(onDone) {
+  console.log('[callback API] Generating key pair')
 
-keygen({
-  comment: 'john@doe.com',
-  read: true
-}, function(err, out){
-	if(err) return console.log('There was a problem : '+err);
-  console.log('Done generating key pairs');
-  console.log(out.key)
-  console.log(out.pubKey)
+  keygen({
+    comment: 'john@doe.com',
+    read: true,
+  }, function (err, out) {
+    if (err) {
+      console.log('There was a problem:', err);
+      process.exitCode = 1;
+    } else {
+      console.log('Done generating key pairs');
+      console.log(out.key);
+      console.log(out.pubKey);
+    }
+    onDone();
+  });
+}
+
+async function runPromiseVersion(onDone) {
+  console.log('[promise API] Generating key pair')
+
+  try {
+    const out = await keygen({
+      comment: 'john@doe.com',
+      read: true,
+    });
+
+    console.log('Done generating key pairs');
+    console.log(out.key);
+    console.log(out.pubKey);
+  } catch (err) {
+    console.log('There was a problem:', err);
+    process.exitCode = 2;
+  } finally {
+    onDone();
+  }
+}
+
+runCallbackVersion(() => {
+  runPromiseVersion(() => { });
 });
